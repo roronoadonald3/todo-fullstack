529bb5fdf0ad9c4abd3d6a867b14b7d94743fc3b
roronoadonald3 <roronoadonald3@gmail.com>
2025-08-04 02:14:28 +0000

    mail done

--- routes/post.js
+++ routes/post.js
@@ -27,7 +27,7 @@ export async function registeruser(req,res){
   password= await hash(password)
   console.log(password)
   db.prepare("INSERT INTO users(id,name,email,password) values (?,?,?,?)").run(Date.now(),name,mail,password)
-  return res.code(302).redirect("/login")
+  return res.redirect("/mail")
   } catch (error) {
     console.log(error)
     return res.code(302).redirect("/signup")
@@ -43,4 +43,58 @@ export async function logout(req,res) {
     res.code(200).send()
     console.log(req.session.user)
   }
+}
+
+export async function Sendmail(req,res,app) {
+  const {email}=req.body
+  let exist=db.prepare("select * from users where email=?").get(email)
+  if(exist){
+   return  res.send({"exist":true})
+  }
+  let randnum = Math.floor(Math.random()*1000000)
+  let subject="Code de verification "
+  let message=`Bonjour cher utilisateur(trice),
+
+Voici votre code de confirmation :
+</p>
+<h1>Code : ${randnum}<h1>
+<p>
+Merci de ne pas partager ce code avec qui que ce soit.
+
+Si vous n'avez pas demandé ce code, veuillez ignorer cet e-mail.
+<br>
+Cordialement,
+RD'S TODOLIST
+
+`
+db.prepare("insert into codes values (?,?)").run(email,randnum)
+let to=email
+console.log(email,to,req.body)
+const info=await app.mailer.sendMail({to,subject,html:`<p>${message}</p>`})
+console.log("mesasage envoyé: ", info.messageId)
+ return res.send({ success: true, redirect: "/mail" })
+
+}
+
+export async function verifycode(req,res) {
+ const parts = await req.parts();
+  const fields = {};
+  
+  for await (const part of parts) {
+    if (part.type === 'file') {
+      
+      continue;
+    }
+    fields[part.fieldname] = part.value;
+  }
+
+  let { code, mail } = fields;
+
+  let vcode=code
+  code=await db.prepare('select code from codes where mail=?').get(mail).code
+  if(Number(vcode)===Number(code)){
+    return res.send("oklm")
+  }
+  console.log(code,mail,vcode)
+  return res.code(404).send("not found")
 }
